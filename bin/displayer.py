079deef19128446332fb5011d2f3809e6c68d9e7
Hugo MARTIN <martin.hugo17@gmail.com>
2016-11-01 12:58:43 +0100

    + enhancement of the configDisplayer

--- bin/displayer.py
+++ bin/displayer.py
@@ -17,11 +17,35 @@ class ConfigDisplayer(QWidget):
     def __init__(self, configuration, colors):
         super().__init__()
         grid = QGridLayout()
-        for marqueur, vehicule in enumerate(configuration.getConfiguration()):
+        # for marqueur, vehicule in enumerate(configuration.getConfiguration()):
+        #     i, j = marqueur//6, marqueur%6
+        #     label = QLabel(str(vehicule))
+        #     label.setStyleSheet("background-color:rgb" + str(colors[str(vehicule)]) + ";qproperty-alignment: AlignCenter;border: 1px solid;");
+        #     grid.addWidget(label, i, j)
+        # self.setLayout(grid)
+
+        print(configuration)
+        posEmpty = list(range(36))
+        for vehicule in configuration.getVehicules():
+            marqueur = vehicule.getMarqueur()
+            size = vehicule.getTypeVehicule()
+            orientation = vehicule.getOrientation()
+
+            posEmpty = [pos for pos in posEmpty if pos not in [occupatePos for occupatePos in range(marqueur, marqueur + size*orientation, orientation)]]
             i, j = marqueur//6, marqueur%6
             label = QLabel(str(vehicule))
-            label.setStyleSheet("background-color:rgb" + str(colors[str(vehicule)]) + ";qproperty-alignment: AlignCenter;border: 1px solid;");
+            label.setStyleSheet("background-color:rgb" + str(colors[str(vehicule)]) + ";qproperty-alignment: AlignCenter;border: 1px solid;")
+            if orientation == Orientation.BAS:
+                grid.addWidget(label, i, j, size, 1)
+            else:
+                grid.addWidget(label, i, j, 1, size)
+
+        for marqueur in posEmpty:
+            i, j = marqueur//6, marqueur%6
+            label = QLabel()
+            label.setStyleSheet("background-color:rgb" + str(colors["0"]) + ";qproperty-alignment: AlignCenter;border: 1px solid;")
             grid.addWidget(label, i, j)
+
         self.setLayout(grid)
 
 

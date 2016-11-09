0ab43c15e202f6153c72978c3543d519946b546c
Hugo MARTIN <martin.hugo17@gmail.com>
2016-11-09 11:58:05 +0100

    + lpsolver modification

--- bin/lpsolver.py
+++ bin/lpsolver.py
@@ -20,17 +20,16 @@ class LPSolver :
         self.marqueurs = range(36)
         self.moves = range(nbMove)
 
-        # correspond à v -> doit être un dictionnaire
+        # correspond à v
         self.initLongueurs(config)
         # correspond à p
-        self.initPositionEntre2Points()
-        # correspond à m -> doit être indicé un dictionnaire
+        self.initPosition2Points()
+        # correspond à m
         self.initPositionsVehicules(config)
-
-        # créé x, y, z
-        self.createDecisionVariable()
-        # self.initDecisionVariable(config) to do
-        # self.createObjectives() to do
+        # créé x, y, z et les initialise selon la configuration passée en param
+        self.createDecisionsVariables(config)
+        # défini la fonction objectif
+        self.createObjectives()
 
 
 
@@ -42,22 +41,46 @@ class LPSolver :
         # [self.matriceMouvement.append(PL.initTab3D(36, 36, self.nbMove)) for i in range(len(config.getVehicules()))]
         
 
-
-
-    def createDecisionsVariables(self):
+    def createDecisionsVariables(self, config):
         """ Créé l'ensemble des variables de décisions nécessaires à la résolution du problème. """
-            self.x, self.y, self.z = {}, {}, {}
-            
-            for vehicule in config.getVehicules():
-                self.x[vehicule.getIdVehicule()] = [[self.model.addVar(vtype=GRB.BINARY) for k in self.moves] for j in self.marqueurs]
-                self.z[vehicule.getIdVehicule()] = [[self.model.addVar(vtype=GRB.BINARY) for k in self.moves] for j in self.marqueurs]
+        self.x, self.y, self.z = {}, {}, {}
 
-                orientation = vehicule.getOrientation()
-                start = vehicule.getMarqueur()%6 if orientation == Orientation.BAS else vehicule.getMarqueur()//6 # peut être l'inverse, ) verifier.
-                possiblesMove = range(start, start + 5*orientation)
-                self.y[vehicule.getIdVehicule()] = [[[self.model.addvar(vtype=GRB.BINARY) for k in self.moves] for i in possiblesMove if i != j] for j in possiblesMove]
+        for vehicule in config.getVehicules():
+            # Création de toutes les variables de décision associé au véhicule 
+            idVehicule = vehicule.getIdVehicule()
+            self.x[idVehicule] = [[self.model.addVar(vtype=GRB.BINARY) for k in self.moves] for j in self.marqueurs]
+            self.z[idVehicule] = [[self.model.addVar(vtype=GRB.BINARY) for k in self.moves] for j in self.marqueurs]
+
+            orientation = vehicule.getOrientation()
+            start = vehicule.getMarqueur()%6 if orientation == Orientation.BAS else vehicule.getMarqueur()//6 # peut être l'inverse, à verifier.
+            possiblesMove = range(start, start + 5*orientation)
+            self.y[idVehicule] = [[[self.model.addvar(vtype=GRB.BINARY) for k in self.moves] for i in possiblesMove if i != j] for j in possiblesMove]
+
+            # Initialisation des variables de décision associé au véhicule
+            self.x[idVehicule][vehicule.getMarqueur][0].X = 1
+            for pos in self.positionsVehicules[idVehicule]:
+                self.z[idVehicule][position][0].X = 1
+
+    def createObjective(self, objectiveType="RHM"):
+        """ Défini l'objectif du modèle en fonction du type d'objectif passé en paramètre.
+            L'objectif est soit de type "RHM", minimisant le nombre de mouvement, soit de type "RHC", minimisant le nombre de case parcourue.
+        """
+        objective = LinExpr()
+        # A OPTIMISER, Développer rapidement parceque flemme et envie de jouer (présaison open omg too op ggwp rito)
+        for vehiculeList in self.y: 
+            for j,marqueurList in enumerate(vehiculeList):
+                for l,deplList in enumerate(marqueurList):
+                    for movementVariable in deplList:
+                        coeff = 1 if objectiveType == "RHM" else len(self.positionEntre2Points(j,l))
+                        objective.addTerm(coeff*movementVariable)
+
+        self.model.setObjective(objective, GRB.MINIMIZE)
                 
-            
+
+
+
+
+
     def setMatricePresence(self, config, step):
         """ 
             Modifie une matrice de la forme : [pour chaque voiture "i" ][pour chaque case "j" ][pour chaque étape "step"]
@@ -87,8 +110,7 @@ class LPSolver :
 
 
     def setMatriceOccupe(self, config, step):
-        """
-            Modifie une matrice de la forme : [pour chaque voiture "i" ][pour chaque case "j" ][pour chaque étape "step"]
+        """ Modifie une matrice de la forme : [pour chaque voiture "i" ][pour chaque case "j" ][pour chaque étape "step"]
             Si une voiture i occupe une case j à une étape donnée "step", la case de la matrice correspondante indiquera 1, 0 sinon
 
             Paramètres : 
@@ -102,8 +124,7 @@ class LPSolver :
                 self.matricePresence[i][ positions ][step] = 1
 
     def getMatriceOccupe(self):
-        """ 
-            Renvoie une matrice de la forme : [pour chaque voiture "i" ][pour chaque case "j" ][pour chaque étape "step"]
+        """ Renvoie une matrice de la forme : [pour chaque voiture "i" ][pour chaque case "j" ][pour chaque étape "step"]
             Si une voiture i occupe une case j à une étape donnée "step", la case de la matrice correspondante indiquera 1, 0 sinon
         """
         return self.matriceOccupe
@@ -219,11 +240,11 @@ class LPSolver :
 
 
 
-    def initPositionEntre2Points(self):
+    def initPosition2Points(self):
         """ Défini la matrice p[][] qui contient pour tout i,j l'ensemble des positions entre ces deux marqueurs.
             Si les cases ne sont pas alignées verticalement ou horizontalement, le tableau renverra une liste vide pour la case correspondante
         """
-        self.p = []
+        self.position2Points = []
         for i in range(36):
             currentList = []
             for j in range(36):
@@ -246,7 +267,7 @@ class LPSolver :
                         positions.append(k) # on ajoute chaque point compris entre i et j
 
                 currentList.append(positions)
-            self.p.append(currentList)
+            self.position2Points.append(currentList)
 
 
     def getPositions2Points(self):

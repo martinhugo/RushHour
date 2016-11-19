272d7592f78447fa275ab85450b93befd46cbbb8
Maud Béguey <ma.be92@hotmail.fr>
2016-11-19 17:27:05 +0100

    1ere version fonctionnelle de Dijkstra
    Pas optimisée et vraiment TRES lente, mais fonctionnelle.
    Délai des 2 minutes non respecté
    Au delà d'une profondeur de 10, devient vraiment très lent.

--- bin/dijkstra.py
+++ bin/dijkstra.py
@@ -1,131 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import math
-from copy import copy
 
 from configuration import *
-
-class Arete:
-
-	def __init__(self, coupleNoeud, poids):
-		self.coupleNoeud = coupleNoeud
-		self.poids = poids
-
-	def getCoupleNoeud(self):
-		""" retourne le couple de noeuds reliés par l'arête """
-		return self.coupleNoeud
-
-	def setCoupleNoeud(self, coupleNoeud):
-		""" permet de modifier le couple de noeuds reliés par l'arête par ceux donnés en paramètre """
-		self.coupleNoeud = coupleNoeud
-
-	def getPoids(self):
-		""" retourne le poids de l'arête """
-		return self.poids
-
-	def setPoids(self, poids):
-		""" modifie le poids de l'arête par celui donné en paramètre """
-		self.poids = poids
-
-
-class Noeud:
-
-	def __init__(self, config):
-		self.config = config
-		self.longueurChemin = math.inf
-		self.historique = []
-
-	def getConfig(self):
-		""" Retourne l'identifiant du noeud """
-		return self.config
-
-	def setLongueurChemin(self, longueurChemin):
-		""" modifie la longueur du chemin par celle donnée en paramètre """
-		self.longueurChemin = longueurChemin
-
-	def getLongueurChemin(self):
-		""" retourne la longueur du chemin """
-		return self.longueurChemin
-
-	def getHistorique(self):
-		""" retourne l'historique du noeud"""
-		return self.historique
-
-	def setHistorique(self, historique):
-		""" modifie complètement l'historique du noeud"""
-		self.historique = historique
-
-	def addHistorique(self, element):
-		""" ajoute un élément à l'historique du noeud"""
-		self.historique.append(element)
-
-
-class Graphe:
-
-	def __init__(self):
-		self.noeuds = []
-		self.aretes = []
-
-	def getNoeuds(self):
-		return self.noeuds
-
-	def getAretes(self):
-		return self.aretes
-
-	def getNoeud(self, config):
-		return [item for item in self.noeuds if item.getConfig() == config]
-
-	def getArete(self, coupleNoeuds):
-		for item in self.aretes:
-			verif1 = Dijkstra.compareDeuxConfig(item.getCoupleNoeud()[0].getConfig(), coupleNoeuds[0].getConfig()) and Dijkstra.compareDeuxConfig(item.getCoupleNoeud()[1].getConfig(), coupleNoeuds[1].getConfig())
-			verif2 = Dijkstra.compareDeuxConfig(item.getCoupleNoeud()[0].getConfig(), coupleNoeuds[1].getConfig()) and Dijkstra.compareDeuxConfig(item.getCoupleNoeud()[1].getConfig(), coupleNoeuds[0].getConfig())
-			if (verif1 or verif2) :
-				return item
-		return []
-
-	def getAretesFromNoeud(self, noeudsDefinitifs):
-		aretesAAnalyser = []
-		for item in self.aretes:
-			if (item.getCoupleNoeud()[0] in noeudsDefinitifs and item.getCoupleNoeud()[1] not in noeudsDefinitifs) or (item.getCoupleNoeud()[1] in noeudsDefinitifs and item.getCoupleNoeud()[0] not in noeudsDefinitifs):
-				aretesAAnalyser.append(item)
-		return aretesAAnalyser
-
-	def addNoeud(self, noeud):
-		self.noeuds.append(noeud)
-
-	def addNoeuds(self, noeuds):
-		for noeud in noeuds:
-			self.addNoeud(noeud)
-
-	def removeNoeud(self, noeud):
-		self.noeuds.remove(noeud)
-
-	def addArete(self, arete):
-		self.aretes.append(arete)
-
-	def addAretes(self, aretes):
-		for arete in aretes:
-			self.addArete(arete)
-
-	@staticmethod
-	def argminArete(listOfAretes):
-		poidsMin = 0
-		for item in listOfAretes:
-			if poidsMin>item.getPoids():
-				poidsMin = item.getPoids()
-		return poidsMin
-
-	@staticmethod
-	def NoeudOppose(noeud, arete):
-		if(arete.getCoupleNoeud()[0]==noeud):
-			return arete.getCoupleNoeud()[0]
-		else:
-			return arete.getCoupleNoeud()[1]
-
-	@staticmethod
-	def coupleNoeud(arete):
-		""" cette fonction retourne les noeuds dans l'ordre suivant : noeud définitif, noeud non définitif pour l'arête donnée"""
-		return arete.getCoupleNoeud()[0], arete.getCoupleNoeud()[1]
+from graphe import *
 
 class Dijkstra:
 	""" première version de l'algo, peu optimisée, et n'enregistrant pas encore l'historique du chemin parcouru, affiche le poids min trouvé """
@@ -136,122 +14,70 @@ class Dijkstra:
 
 	def launchDijkstra(self, config, nbMax):
 
-		dernierNoeudVisite = Noeud(config) 
-		self.A.append(dernierNoeudVisite)
-		self.graphe.addNoeud(dernierNoeudVisite)
+		configPoidsMin = Noeud(config)
+		configPoidsMin.setLongueurChemin(0)
+		self.graphe.addNoeud(configPoidsMin)
+
+		while(not self.verifCondition(configPoidsMin.getConfig())):
+			self.A.append(configPoidsMin)
+
+			self.graphe.constructNoeuds(configPoidsMin, configPoidsMin.getConfig().getPossiblePosition())
+
+			# initialisation
+			aretesAAnalyser = self.graphe.getAretesFromNoeud(self.A) # toutes les aretes entre A et non A
+			poidsMin = math.inf
+			noeudCheminMin = None
+
+			for arete in aretesAAnalyser:
+				# on prend les extrémités de l'arete
+				noeudA, noeudNotA = self.graphe.coupleNoeud(arete, self.A)
+
+				# on met à jour la taille du chemin de noeud en dehors de A
+				noeudNotA = self.majLongueurChemin(arete, noeudNotA, noeudA)
+
+				# on met à jour poids min
+				poidsMin, verif = self.majPoidsMin(poidsMin, arete, noeudNotA, noeudA)
+
+				# si poids min mis à jour, on a rencontré un chemin plus court, que l'on stocke
+				if(verif):
+					noeudCheminMin = noeudNotA
+
+			# on récupère le chemin le plus court et on itère
+			configPoidsMin = noeudCheminMin
+			print("longueur --->", noeudCheminMin.getLongueurChemin())
+
+			print("poids chemin considéré : ", noeudCheminMin.getLongueurChemin())
+
+		print("\nlongueur minimale trouvée : ", configPoidsMin.getLongueurChemin(), "\n")
+		print(configPoidsMin.getConfig())
+
+	@staticmethod
+	def verifCondition(config):
+		""" si le noeud sélectionné permet de conclure qu'on a fini, ie voiture "g" en position 16"""
 		
-		# on fixe la longueur du chemin au noeud de départ à 0
-		dernierNoeudVisite.setLongueurChemin(0)
-
-		# tant qu'on a pas un chemin définitif vers l'objectif ou dépassé le nb de coup max
-		while(not Dijkstra.verifCondition(dernierNoeudVisite)):
-
-			# dictionnaire stockant pour chaque véhicule, la liste de ses mvts possibles
-			dicoNoeudsPotentiels = dernierNoeudVisite.getConfig().getPossiblePosition() 
-
-			# complétion de l'arbre à partir du dernier noeud ajouté (le reste n'a pas besoin d'être modifié, et reste fixe)
-			if(dernierNoeudVisite.getLongueurChemin()<nbMax):
-				self.constructNoeuds(dernierNoeudVisite, dicoNoeudsPotentiels)
-
-			# liste des arêtes dont on cherche à déterminer le poids min
-			listOfAretes = self.graphe.getAretesFromNoeud(self.A)
-
-			#initialisation des variables utilisées dans l'algo
-			currentNoeudA = None # noeud de liste A considéré
-			currentNoeudnotA = None # noeud considéré pas dans A
-			currentNoeudMin = None # noeud de poids min considéré
-
-			poidsMin = math.inf # poids du chemin minimum
-
-			for arete in listOfAretes:
-
-				currentNoeudMin = currentNoeudnotA # initialisation si aucune amélioration de longueur
-
-				# on prend, pour l'arete courant, les deux noeuds reliés
-				currentNoeudA, currentNoeudnotA = self.graphe.coupleNoeud(arete)
-
-				# si le chemin en passant par cette arête est plus petit que ceux obtenus avant
-
-				if((currentNoeudA.getLongueurChemin() + arete.getPoids()) < poidsMin):
-					poidsMin = currentNoeudA.getLongueurChemin() + arete.getPoids()
-					currentNoeudMin = currentNoeudnotA
-
-				# si le chemin jusqu'à ce noeud est plus petit que celui déjà enregistré
-				if((currentNoeudA.getLongueurChemin() + arete.getPoids()) < currentNoeudnotA.getLongueurChemin()):
-					
-					currentNoeudnotA.setLongueurChemin(currentNoeudA.getLongueurChemin() + arete.getPoids())
-					# ici pour modifier historique ---> prochaine itération
-
-			dernierNoeudVisite = currentNoeudMin
-
-			# on ajoute le noeud de chemin min à la liste définitive des chemins
-			self.A.append(dernierNoeudVisite)
-			# print("poids chemin considéré : ", dernierNoeudVisite.getLongueurChemin())
-			# print(dernierNoeudVisite.getConfig().getConfiguration())
-
-		print("\nlongueur minimale trouvée : ", dernierNoeudVisite.getLongueurChemin(), "\n")
-
-	@staticmethod
-	def newConfig(configInit, vehicle, newPosition):
-		""" crée une nouvelle config à partir d'un véhicule et d'une nouvelle position, retourne la nouvelle config"""
-
-		newConfig = copy(configInit)
-		newVehicle = copy(vehicle)
-		newVehicle.setMarqueur(newPosition)
-
-		listVehicles = copy(configInit.getVehicules())
-
-		listVehicles.remove(vehicle)
-		listVehicles.append(newVehicle)
-
-		newConfig.setVehicules(listVehicles)
-		newConfig.constructConfiguration()
-
-		return newConfig
-
-	@staticmethod
-	def verifCondition(noeud):
-		""" si le noeud sélectionné permet de conclure qu'on a fini, ie voiture "g" en position 17"""
-		config = noeud.getConfig()
 		vehicules = config.getVehicules()
 		vehiculeG = None
 		for vehicule in vehicules:
 			if(vehicule.getIdVehicule() == 'g'): # -------> A modifier par la suite
 				vehiculeG = vehicule
-		if(vehiculeG.getMarqueur() == 17):
+		if(vehiculeG.getMarqueur() == 16):
 			return True
 		return False
 
-	def constructNoeuds(self, noeud, dico):
-		listNoeuds = []
-		
-		for vehicule in dico.keys(): # pour chaque véhicule
+	def majLongueurChemin(self, arete, noeud, noeudDefinitif):
+		""" retourne le noeud mis à jour si le chemin est plus court"""
+		if((noeudDefinitif.getLongueurChemin() + arete.getPoids()) < noeud.getLongueurChemin()):
+			noeud.setLongueurChemin(noeudDefinitif.getLongueurChemin() + arete.getPoids())
+			# noeud.setHistorique(noeudDefinitif.getConfig())
+		return noeud
 
-			if(dico[vehicule] != None): # si mouvements possibles pour le véhicule
+	def majPoidsMin(self, poidsMin, arete, noeud, noeudDefinitif):
+		""" retourne poidsMin si le poidsMin est plus petit que celui fournit en paramètre"""
+		if((noeudDefinitif.getLongueurChemin() + arete.getPoids()) < poidsMin):
+			poidsMin = noeudDefinitif.getLongueurChemin() + arete.getPoids()
+			return poidsMin, True
+		return poidsMin, False
 
-				for marqueurPossible in dico[vehicule]: # pour chaque position possible du véhicule
-					noeudToAdd = Noeud(Dijkstra.newConfig(noeud.getConfig(), vehicule, marqueurPossible)) # définition du noeud
-
-					if(not Dijkstra.compareDeuxConfig(noeudToAdd.getConfig(), noeud.getConfig())): # si noeud inexistant
-						noeudToAdd.setLongueurChemin(math.inf) # chemin fixé au max
-						self.graphe.addNoeud(noeudToAdd) # chemin ajouté au graphe
-					else:
-						print("test")
-
-					if(self.graphe.getArete([noeudToAdd, noeud]) == []): # si arete inexistante
-						self.graphe.addArete(Arete([noeud, noeudToAdd], 1)) # arete ajoutée au graphe avec aret1 dans A, l'autre non
-
-
-
-	@staticmethod
-	def compareDeuxConfig(config1, config2):
-		list1 = []
-		list2 = []
-		for vehicule in range(len(config1.getVehicules())):
-			list1.append(config1.getVehicules()[vehicule].getMarqueur())
-		for vehicule in range(len(config2.getVehicules())):
-			list2.append(config2.getVehicules()[vehicule].getMarqueur())
-		return (len(ListTools.intersection(list1, list2)) == len(config1.getVehicules()))
 
 
 def main():
@@ -262,7 +88,6 @@ def main():
 
 	noeud = Noeud(conf)
 	dijkstra.launchDijkstra(conf, 8)
-
 
 main()
 
---
+++ bin/graphe.py
@@ -0,0 +1,251 @@
+# -*- coding: utf-8 -*-
+
+import math
+from configuration import *
+from copy import copy
+
+
+
+
+
+
+
+
+
+
+
+
+class Arete:
+
+	def __init__(self, coupleNoeud, poids):
+		self.coupleNoeud = coupleNoeud
+		self.poids = poids
+
+	def getCoupleNoeud(self):
+		""" retourne le couple de noeuds reliés par l'arête """
+		return self.coupleNoeud
+
+	def setCoupleNoeud(self, coupleNoeud):
+		""" permet de modifier le couple de noeuds reliés par l'arête par ceux donnés en paramètre """
+		self.coupleNoeud = coupleNoeud
+
+	def getPoids(self):
+		""" retourne le poids de l'arête """
+		return self.poids
+
+	def setPoids(self, poids):
+		""" modifie le poids de l'arête par celui donné en paramètre """
+		self.poids = poids
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Noeud:
+
+	def __init__(self, config):
+		self.config = config
+		self.longueurChemin = math.inf
+		self.historique = []
+		self.aretesReliees = []
+
+	def getConfig(self):
+		""" Retourne la configuration correpondant au noeud """
+		return self.config
+
+	def setLongueurChemin(self, longueurChemin):
+		""" modifie la longueur du chemin par celle donnée en paramètre """
+		self.longueurChemin = longueurChemin
+
+	def getLongueurChemin(self):
+		""" retourne la longueur du chemin """
+		return self.longueurChemin
+
+	def getHistorique(self):
+		""" retourne l'historique du noeud"""
+		return self.historique
+
+	def setHistorique(self, historique):
+		""" modifie l'historique du noeud"""
+		self.historique = historique
+
+	def addAreteReliee(self, arete):
+		self.aretesReliees.append(arete)
+
+	def getNoeudsRelies(self):
+		return self.noeudsRelies
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Graphe:
+
+	def __init__(self):
+		self.noeuds = []
+		self.aretes = []
+
+
+	############################### GETTERS ET SETTERS #########################################
+
+	def getNoeuds(self):
+		""" retourne la liste des noeuds du graphe"""
+		return self.noeuds
+
+	def getAretes(self):
+		""" retourne la liste des aretes du graphe"""
+		return self.aretes
+
+	def getNoeud(self, config):
+		""" retourne les noeuds selon la config donnée en paramètre"""
+		return [item for item in self.noeuds if self.compare2Config(item.getConfig(), config)]
+
+	def getArete(self, coupleNoeuds):
+		""" retourne l'arete correspondant au couple de noeuds fournit en argument"""
+		arete = Arete(coupleNoeuds)
+		for item in self.aretes:
+			if (Graphe.compare2Arete(item, arete)):
+				return item
+		return []
+
+	def getAretesFromNoeud(self, noeudsDefinitifs):
+		aretesAAnalyser = []
+		for item in self.aretes:
+			if ((self.isInList(item.getCoupleNoeud()[0], noeudsDefinitifs) and not (self.isInList(item.getCoupleNoeud()[1], noeudsDefinitifs))) or (self.isInList(item.getCoupleNoeud()[1], noeudsDefinitifs) and not (self.isInList(item.getCoupleNoeud()[0], noeudsDefinitifs)))):
+				aretesAAnalyser.append(item)
+		return aretesAAnalyser
+
+	######################################## AUTRES METHODES ###################################
+
+	def addNoeud(self, noeud):
+		self.noeuds.append(noeud)
+
+	def addNoeuds(self, noeuds):
+		for noeud in noeuds:
+			self.addNoeud(noeud)
+
+	def removeNoeud(self, noeud):
+		self.noeuds.remove(noeud)
+
+	def addArete(self, arete):
+		self.aretes.append(arete)
+
+	def addAretes(self, aretes):
+		for arete in aretes:
+			self.addArete(arete)
+
+	def constructNoeuds(self, noeud, dico):
+		""" construit des noeuds en fonction d'un dictionnaire passé en paramètre"""
+		listNoeuds = []
+		for objet in dico.keys(): # pour chaque véhicule
+			if(dico[objet] != None):
+				for marqueur in dico[objet]: # pour chaque position possible du véhicule
+					noeudToAdd = Noeud(self.newConfig(noeud.getConfig(), objet, marqueur)) # définition du noeud
+					self.constructNoeud(noeud, noeudToAdd)
+					
+	def constructNoeud(self, noeudDefinitif, noeudToAdd):
+		""" ajoute le noeud et l'arête correspondant dans le graphe si le noeud et l'arete ne sont pas déjà dans le graphe"""
+
+		if(not self.verifNoeudInGraphe(noeudToAdd)): # si noeud inexistant
+			noeudToAdd.setLongueurChemin(math.inf)
+			self.addNoeud(noeudToAdd)
+
+		areteToAdd = Arete([noeudDefinitif, noeudToAdd], 1)
+		if(not self.verifAreteInGraphe(areteToAdd)): # si arete inexistante
+			self.addArete(areteToAdd)
+
+	def verifNoeudInGraphe(self, noeud):
+		""" retourne vrai si le noeud est dans le graphe"""
+
+		for n in self.getNoeuds():
+			if(self.compare2Config(n.getConfig(), noeud.getConfig())):
+				return True
+		return False
+
+	def verifAreteInGraphe(self, arete):
+		""" retourne vrai si l'arete est dans le graphe"""
+		noeud1, noeud2 = arete.getCoupleNoeud()
+		for a in self.getAretes():
+			if(self.compare2Aretes(a, arete)):
+				return True
+		return False
+
+	####################################### METHODES STATIQUES #########################################
+
+	@staticmethod
+	def compare2Config(config1, config2):
+		""" cette méthode retourne vrai si les 2 configurations données en paramètre sont identiques"""
+		list1 = []
+		list2 = []
+		for vehicule in range(len(config1.getVehicules())):
+			list1.append(config1.getVehicules()[vehicule].getMarqueur())
+		for vehicule in range(len(config2.getVehicules())):
+			list2.append(config2.getVehicules()[vehicule].getMarqueur())
+		return (len(ListTools.intersection(list1, list2)) == len(config1.getVehicules()))
+
+	@staticmethod
+	def compare2Noeuds(noeud1, noeud2):
+		""" cette fonction retourne vrai si les 2 noeuds donnés en paramètres ont la meme config"""
+		return Graphe.compare2Config(noeud1.getConfig(), noeud2.getConfig())
+
+	@staticmethod
+	def compare2Aretes(arete1, arete2):
+		""" cette fonction retourne vrai si les aretes données en paramètre sont identiques"""
+		noeud11, noeud12 = arete1.getCoupleNoeud()
+		noeud21, noeud22 = arete2.getCoupleNoeud()
+		if(Graphe.compare2Noeuds(noeud11, noeud21) and Graphe.compare2Noeuds(noeud12, noeud22)):
+			return True
+		if(Graphe.compare2Noeuds(noeud12, noeud21) and Graphe.compare2Noeuds(noeud11, noeud22)):
+			return True
+		return False
+
+	@staticmethod
+	def isInList(noeud, listNoeud):
+		""" retourne vrai si le noeud est dans la liste de noeud donnée en paramètre"""
+		return True in [Graphe.compare2Noeuds(noeud, i) for i in listNoeud]
+
+	@staticmethod
+	def coupleNoeud(arete, noeudDefinitifs):
+		""" cette fonction retourne les noeuds dans l'ordre suivant : noeud définitif, noeud non définitif pour l'arête donnée"""
+		if(Graphe.isInList(arete.getCoupleNoeud()[0], noeudDefinitifs)):
+			return arete.getCoupleNoeud()[0], arete.getCoupleNoeud()[1]
+		return arete.getCoupleNoeud()[1], arete.getCoupleNoeud()[0]
+
+	@staticmethod
+	def getConfigCoupleNoeud(arete):
+		""" retourne les configs des noeuds reliés par l'arete"""
+		return arete.getCoupleNoeud()[0].getConfig(), arete.getCoupleNoeud()[1].getConfig()
+
+	@staticmethod
+	def newConfig(configInit, vehicle, newPosition):
+		""" crée une nouvelle config à partir d'un véhicule et d'une nouvelle position, retourne la nouvelle config"""
+
+		newConfig = copy(configInit)
+		newVehicle = copy(vehicle)
+		newVehicle.setMarqueur(newPosition)
+
+		listVehicles = copy(configInit.getVehicules())
+
+		listVehicles.remove(vehicle)
+		listVehicles.append(newVehicle)
+
+		newConfig.setVehicules(listVehicles)
+		newConfig.constructConfiguration()
+
+		return newConfig

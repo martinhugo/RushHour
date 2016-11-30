f61e27046ecf92a39a5078d6aedcbf968fdc5b0a
Maud Béguey <ma.be92@hotmail.fr>
2016-11-30 21:19:05 +0100

    + added comments

--- bin/dijkstra.py
+++ bin/dijkstra.py
@@ -7,9 +7,15 @@ from configuration import *
 from graphe import *
 
 class Dijkstra:
-    """ """
+   """ Cette classe permet la résolution d'une grille de RushHour par l'algorithme de Dijkstra"""
 
     def __init__(self, config, nbMax, flag = "RHM"):
+    	""" 
+    	Params : 
+    		config -> la configuration initiale
+    		nbMax -> le nombre maximum de déplacements autorisés
+    		flag (facultatif) -> objectif de résolution, par défaut sur RHM
+    	"""
         self.graphe = Graphe()
         self.config = config
         self.nbMax = nbMax
@@ -27,30 +33,32 @@ class Dijkstra:
         return self.solution
 
     def solve(self, flag = "RHM"):
-        """ Retourne la liste des configurations permettant de résoudre la configuration.
-            Cette configuration est retournée par l'algorithme de Dijkstra
+        """
+        	Cette méthode permet la résolution de la grille de RushHour par Dijkstra
             La résolution peut être faite selon RHM ou RHC selon la valeur du flag.
+            Params : flag(facultatif) -> objectif de résolution, par défaut sur RHM
+            Return : liste des configurations permettant de résoudre la grille de RushHour
         """
         start_time = time.time()
 
-        noeudsPoidsMin = Noeud(self.getConfig())
-        noeudsPoidsMin.setHistorique([None, 0, 0])
+        noeudsPoidsMin = Noeud(self.getConfig()) # correspond au noeud initial
+        noeudsPoidsMin.setHistorique([None, 0, 0]) # historique de ce noeud, sans parent, avec un chemin de 0 et une profondeur de 0
         self.graphe.addNoeud(noeudsPoidsMin) # ajout du noeud dans le graphe
-        self.nonDefinitiveEdges.append(noeudsPoidsMin) # ajout du premier noeud dans B (tant que pas dans la boucle, pas définitif)
+        self.nonDefinitiveEdges.append(noeudsPoidsMin) # ajout du premier noeud dans nonDefinitiveEdges (tant que pas dans la boucle, pas définitif)
 
         while(not noeudsPoidsMin == None and not Configuration.verifCondition(noeudsPoidsMin.getConfig()) and self.getNbMax() > noeudsPoidsMin.getProfondeur()):
 
-            self.nonDefinitiveEdges.remove(noeudsPoidsMin) # on retire le noeud définitif de B
+            self.nonDefinitiveEdges.remove(noeudsPoidsMin) # on retire le noeud définitif de nonDefinitiveEdges
             self.graphe.constructNoeuds(noeudsPoidsMin, self.nonDefinitiveEdges, flag) # ajout des noeuds inexistants et maj de la taille du chemin
 
             # initialisation
             poidsMin = math.inf
             noeudCheminMin = None
 
-            for noeud in self.nonDefinitiveEdges: # pour chaque noeud dans B
+            for noeud in self.nonDefinitiveEdges: # pour chaque noeud dans nonDefinitiveEdges
                 if(noeud.getLongueurChemin() < poidsMin):
-                    poidsMin = noeud.getLongueurChemin()
-                    noeudCheminMin = noeud
+                    poidsMin = noeud.getLongueurChemin() # on garde le poids min
+                    noeudCheminMin = noeud # on garde le noeud non définitif de poids min
 
             noeudsPoidsMin = noeudCheminMin # on récupère le chemin le plus court et on itère
 
@@ -71,14 +79,6 @@ class Dijkstra:
         # si la solution ne peut être trouvé en nombre de pas
         else:
             return []
-
-    @staticmethod
-    def majPoidsMin(poidsMin, noeud, noeudCheminMin):
-        """ retourne poidsMin si le poidsMin est plus petit que celui fournit en paramètre """
-        if(noeud.getLongueurChemin() < poidsMin):
-            poidsMin = noeud.getLongueurChemin()
-            noeudCheminMin = noeud
-        return poidsMin, noeudCheminMin
 
 if __name__ == "__main__":
     conf = Configuration.readFile("../puzzles/avancé/jam26.txt")
--- bin/graphe.py
+++ bin/graphe.py
@@ -13,45 +13,75 @@ from configuration import *
 
 
 class Noeud:
+	""" Cette classe permet la création d'un noeud, avec un historique, un identifiant unique et une configuration"""
 
 	def __init__(self, config):
+		""" Params : config -> correspond à la configuration du noeud"""
 		self.config = config
 		self.id = str(config)
 		self.historique = [None, None, None] # se présente sous la forme [pere, taille du chemin, profondeur]
 
 	def getId(self):
+		""" Permet de récupérer l'identifiant du noeud
+			Params : None
+			Return : l'identifiant du noeud
+		"""
 		return self.id
 
 
 	def getConfig(self):
-		""" Retourne la configuration correpondant au noeud """
+		""" Permet de récupérer la configuration du noeud
+			Params : None
+			Return : la configuration du noeud
+		"""
 		return self.config
 
 
 	def getHistorique(self):
-		""" retourne l'historique du noeud"""
+		""" Permet d'obtenir l'historique du noeud
+			Params : None
+			Return : la liste correspondant à l'historique du noeud
+		"""
 		return self.historique
 
 
 	def setHistorique(self, historique):
-		""" modifie l'historique du noeud"""
+		""" Modifie l'historique du noeud avec l'historique donnée en paramètre
+			Params : historique
+			Return : None
+		"""
 		self.historique = historique
 
 
 	def getParent(self):
+		""" Récupère la référence du père du noeud
+			Params : None
+			Return : Noeud pere
+		"""
 		return self.historique[0]
 
 
 	def setParent(self, pere):
+		""" Permet de modifier le père du noeud
+			Params : pere du noeud
+			Return : None
+		"""
 		self.historique[0] = pere
 
 
 	def getLongueurChemin(self):
-		""" retourne la longueur du chemin """
+		""" Permet d'obtenir la longueur du chemin 
+			Params : None
+			Return : longueur du chemin pour atteindre le noeud
+		"""
 		return self.historique[1]
 
 
 	def setLongueurChemin(self, longueurChemin):
+		""" Permet de modifier l'historique du noeud
+			Params : longueurChemin
+			Return : None
+		"""
 		self.historique[1] = longueurChemin
 
 
@@ -86,8 +116,10 @@ class Noeud:
 
 
 class Graphe:
+	""" Cette classe permet la création d'un graphe contenant une liste de noeuds"""
 
 	def __init__(self):
+		""" Params : None"""
 		self.noeuds = []
 
 
@@ -97,11 +129,21 @@ class Graphe:
 
 
 	def addNoeud(self, noeud):
+		""" Cette fonction ajoute un noeud au graphe
+			Params : Le noeud à ajouter
+			Return : None
+		"""
 		self.noeuds.append(noeud)
 			
 
 	def constructNoeuds(self, noeud, nonDefinitveEdges, flag = "RHM"):
-		""" construit des noeuds en fonction d'un dictionnaire passé en paramètre"""
+		""" construit des noeuds à partir du noeud passé en paramètre
+			Params: noeud -> noeud à partir duquel on souhaite construire les autres noeuds
+					nonDefinitiveEdges -> liste des noeuds non définitifs
+					flag (facultatif) -> objetcif de résolution du problème, par défaut RHM
+			Return : None
+		"""
+
 		movements = noeud.getConfig().getPossibleMovements()
 		poids = 1
 		# pour chaque véhicule, pour chaque position possible du véhicule
@@ -128,7 +170,14 @@ class Graphe:
 
 
 	def constructNoeud(self, noeudDefinitif, noeudToAdd, nonDefinitveEdges, poids):
-		""" ajoute le noeud et l'arête correspondant dans le graphe si le noeud et l'arete ne sont pas déjà dans le graphe"""
+		""" ajoute le noeud et l'arête correspondant dans le graphe
+			Params : noeudDefinitif auquel relier le noeud à créer
+					 noeudToAdd : le noeud à ajouter au graphe
+					 nonDefinitifEdges : liste de noeuds dont le chemin min n'est pas encore définitif
+					 poids : poids du chemin du noeud définitif au noeud à ajouter
+			Return : None
+		"""
+
 		noeudToAdd.setHistorique([noeudDefinitif, noeudDefinitif.getLongueurChemin() + poids, noeudDefinitif.getProfondeur() + 1])
 		self.addNoeud(noeudToAdd)
 		nonDefinitveEdges.append(noeudToAdd)
@@ -147,7 +196,10 @@ class Graphe:
 
 	@staticmethod
 	def countConfig(conf, nbPas):
-		""" compte le nombre de configuration atteignables en nbPas pas depuis conf"""
+		""" compte le nombre de configuration atteignables en i pas depuis conf
+			Params : Nombre de pas souhaités
+			Return : nombre de configurations accessibles
+		"""
 
 		graphe = Graphe()
 		config = Noeud(conf)

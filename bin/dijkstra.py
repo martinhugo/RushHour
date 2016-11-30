2ff008d19010ff3abbaf8b93803d29408c71c853
Maud Béguey <ma.be92@hotmail.fr>
2016-11-30 21:09:14 +0100

    + optimisation historique

--- bin/dijkstra.py
+++ bin/dijkstra.py
@@ -4,7 +4,7 @@ import math
 import time
 
 from configuration import *
-from graphe_hm import *
+from graphe import *
 
 class Dijkstra:
     """ """
@@ -34,11 +34,11 @@ class Dijkstra:
         start_time = time.time()
 
         noeudsPoidsMin = Noeud(self.getConfig())
-        noeudsPoidsMin.setHistorique([[self.getConfig(), 0]])
+        noeudsPoidsMin.setHistorique([None, 0, 0])
         self.graphe.addNoeud(noeudsPoidsMin) # ajout du noeud dans le graphe
         self.nonDefinitiveEdges.append(noeudsPoidsMin) # ajout du premier noeud dans B (tant que pas dans la boucle, pas définitif)
 
-        while(not noeudsPoidsMin == None and not Configuration.verifCondition(noeudsPoidsMin.getConfig()) and self.getNbMax() > (len(noeudsPoidsMin.getHistorique())-1)):
+        while(not noeudsPoidsMin == None and not Configuration.verifCondition(noeudsPoidsMin.getConfig()) and self.getNbMax() > noeudsPoidsMin.getProfondeur()):
 
             self.nonDefinitiveEdges.remove(noeudsPoidsMin) # on retire le noeud définitif de B
             self.graphe.constructNoeuds(noeudsPoidsMin, self.nonDefinitiveEdges, flag) # ajout des noeuds inexistants et maj de la taille du chemin
@@ -58,10 +58,15 @@ class Dijkstra:
         print("temps total ---->", round(stop_time - start_time, 2), " sec")
 
         # si on a trouvé une solution
-        if (noeudsPoidsMin is not None) and (self.getNbMax() >= len(noeudsPoidsMin.getHistorique())):
-            listConfig = [noeudsPoidsMin.getHistorique()[i][0] for i in range(len(noeudsPoidsMin.getHistorique()))]
+        if (noeudsPoidsMin is not None) and (self.getNbMax() >= noeudsPoidsMin.getProfondeur()):
+
+            listConfig = [noeudsPoidsMin.getConfig()]
+            while(noeudsPoidsMin.getParent() != None):
+            	noeudsPoidsMin = noeudsPoidsMin.getParent()
+            	listConfig.append(noeudsPoidsMin.getConfig())
+
             listConfig.reverse()
-            return listConfig   # si on a pas trouvé de solution en un nombre de pas < nbMax
+            return listConfig
 
         # si la solution ne peut être trouvé en nombre de pas
         else:
@@ -76,14 +81,11 @@ class Dijkstra:
         return poidsMin, noeudCheminMin
 
 if __name__ == "__main__":
-    conf = Configuration.readFile("../puzzles/débutant/jam2.txt")
+    conf = Configuration.readFile("../puzzles/avancé/jam26.txt")
     dijkstra = Dijkstra(conf, 52, flag = "RHM")
     print(len(dijkstra.getSolution()))
     # dijkstra.launchDijkstra(conf, 35, flag = "RHM")
-    # print("nombre de configurations atteignables en 15 pas -->", Graphe.countConfig(conf, 15))
-
-
-
+    # print("nombre de configurations atteignables en 1 pas -->", Graphe.countConfig(conf, 1))
 
 
 
--- bin/graphe.py
+++ bin/graphe.py
@@ -17,7 +17,7 @@ class Noeud:
 	def __init__(self, config):
 		self.config = config
 		self.id = str(config)
-		self.historique = []
+		self.historique = [None, None, None] # se présente sous la forme [pere, taille du chemin, profondeur]
 
 	def getId(self):
 		return self.id
@@ -26,15 +26,6 @@ class Noeud:
 	def getConfig(self):
 		""" Retourne la configuration correpondant au noeud """
 		return self.config
-
-
-	def getLongueurChemin(self):
-		""" retourne la longueur du chemin """
-		return self.historique[-1][1]
-
-
-	def setLongueurChemin(self, longueurChemin):
-		self.historique[-1][1] = longueurChemin
 
 
 	def getHistorique(self):
@@ -46,10 +37,35 @@ class Noeud:
 		""" modifie l'historique du noeud"""
 		self.historique = historique
 
+
+	def getParent(self):
+		return self.historique[0]
+
+
+	def setParent(self, pere):
+		self.historique[0] = pere
+
+
+	def getLongueurChemin(self):
+		""" retourne la longueur du chemin """
+		return self.historique[1]
+
+
+	def setLongueurChemin(self, longueurChemin):
+		self.historique[1] = longueurChemin
+
+
+	def getProfondeur(self):
+		return self.historique[2]
+
+
+	def setProfondeur(self, longueurChemin):
+		self.historique[2] = longueurChemin
+
+
 	def __eq__(self, noeud2):
 		""" cette fonction retourne vrai si les 2 noeuds donnés en paramètres ont la meme config"""
 		return self.getId() == noeud2.getId() if noeud2 != None else None
-
 
 
 	@staticmethod
@@ -102,17 +118,18 @@ class Graphe:
 
 				currentEdge = self.verifNoeudInGraphe(Noeud(Configuration.newConfig(noeud.getConfig(), vehicle, marqueur)))
 
-				if(len(currentEdge.getHistorique()) == 0): # si le noeud n'a jamais été crée
+				if(currentEdge.getProfondeur() == None): # si le noeud n'a jamais été crée
 					self.constructNoeud(noeud, currentEdge, nonDefinitveEdges, poids)
 
 				elif(currentEdge.getLongueurChemin() > noeud.getLongueurChemin() + poids): # si noeud déjà créé et chemin plus petit possible
 					currentEdge.setLongueurChemin(noeud.getLongueurChemin() + poids)
+					currentEdge.setParent(noeud)
 				
 
 
 	def constructNoeud(self, noeudDefinitif, noeudToAdd, nonDefinitveEdges, poids):
 		""" ajoute le noeud et l'arête correspondant dans le graphe si le noeud et l'arete ne sont pas déjà dans le graphe"""
-		noeudToAdd.setHistorique(noeudDefinitif.getHistorique() + [[noeudToAdd.getConfig(), noeudDefinitif.getLongueurChemin() + poids]])
+		noeudToAdd.setHistorique([noeudDefinitif, noeudDefinitif.getLongueurChemin() + poids, noeudDefinitif.getProfondeur() + 1])
 		self.addNoeud(noeudToAdd)
 		nonDefinitveEdges.append(noeudToAdd)
 
@@ -128,19 +145,21 @@ class Graphe:
 
 
 
-	### METHODE INUTILE ??
 	@staticmethod
-	def countConfig(conf, i):
-		""" compte le nombre de configuration atteignables en i pas depuis conf"""
+	def countConfig(conf, nbPas):
+		""" compte le nombre de configuration atteignables en nbPas pas depuis conf"""
 
 		graphe = Graphe()
-		flag = "RHM"
 		config = Noeud(conf)
-		config.setHistorique([[conf, 0]])
+		config.setHistorique([conf, 0, 0])
 		A = [config]
 
-		while(not config == None and i > (len(config.getHistorique())-1)):
+		while(config != None and nbPas > config.getProfondeur()):
+			print(config.getProfondeur())
 			A.remove(config) # on retire le noeud définitif de A
-			graphe.constructNoeuds(config, A, flag) # ajout des noeuds inexistants et maj de la taille du chemin
-			config = A[0]
-		return len(graphe.getNoeuds())-1
+			graphe.constructNoeuds(config, A) # ajout des noeuds inexistants et maj de la taille du chemin
+			if len(A) > 0: # si on est pas au dernier élément
+				config = A[0]
+			else:
+				config = None
+		return len(graphe.getNoeuds())
